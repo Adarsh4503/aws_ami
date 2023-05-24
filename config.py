730ea4a505a98fa3bd27e24b369aa4ca58eb4806
Adarsh <78885943+Adarsh4503@users.noreply.github.com>
2023-05-24 19:12:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+aws_access_key_id=""
+aws_secret_access_key=""
+region="ap-south-1"
+bucket="examplebucket"
+image_format="VMDK"     
+grantee_id = 'c4d8eabf8db69dbe46bfe0e517100c554f01200b104d59cd408e777ba442a322'
